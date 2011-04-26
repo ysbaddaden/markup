8dad8e1e583ad896ea9598a1dd64ee9a3a14c7ca
Julien Portalier <ysbaddaden@gmail.com>
2011-04-26 08:55:42 +0200

    to_html now accepts a :indent => true option for indenting the output HTML.

--- lib/markup.rb
+++ lib/markup.rb
@@ -45,21 +45,30 @@ class Markup
     end.compact
   end
 
-  def to_html
-    str = _to_html(parse)
+  def to_html(options = {})
+    str = _to_html(parse, options).strip
     str.respond_to?(:html_safe) ? str.html_safe : str
   end
 
   protected
-    def _to_html(struct)
-      struct.collect do |tag, struct|
+    def _to_html(struct, options = {})
+      deep = options[:deep] || 0
+      
+      html = struct.collect do |tag, struct|
         if tag.is_a?(String)
           tag
         else
-          content = struct.is_a?(Array) ? _to_html(struct) : struct
+          content = struct.is_a?(Array) ? _to_html(struct, options.merge(:deep => deep + 1)) : struct
           "<#{tag}>#{content}</#{tag}>"
         end
-      end.join
+      end
+      
+      if options[:indent]
+        indent  = "  "
+        html.insert(0, "").join("\n#{indent * deep}") + "\n" + (indent * [0, deep - 1].max)
+      else
+        html.join
+      end
     end
 
     def parse_blocks(text)
--- test/markup_test.rb
+++ test/markup_test.rb
@@ -188,5 +188,30 @@ EOV
           "</ul>" +
         "</li>" +
       "</ol>", Markup.new(text).to_html
+    
+    assert_equal "<h1>Heading 1</h1>
+<p>The very first paragraph.</p>
+<h2>Heading 2</h2>
+<ol>
+  <li>
+    item 1
+    <ul>
+      <li>
+        <p>A first paragraph inside a nested list.</p>
+        <p>A second paragraph inside a nested list.</p>
+      </li>
+    </ul>
+  </li>
+  <li>
+    item 2
+    <ul>
+      <li>2.a</li>
+      <li>
+        <p>Another paragraph inside a nested list.</p>
+        <p>Yet another paragraph.</p>
+      </li>
+    </ul>
+  </li>
+</ol>", Markup.new(text).to_html(:indent => true)
   end
 end
